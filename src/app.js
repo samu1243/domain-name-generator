31fa10000a9c5dfb14e0f9d4afe927428ab4dfc1
samu1243 <samueldjekki@gmail.com>
2022-04-11 18:09:41 -0400

    creacion de array para domain name

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let exten = [".net", ".com", ".org", ".us"];
+
+//write your code here
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let o = 0; o < exten.length; o++) {
+        let resultado = `${pronoun[i]}${adj[j]}${noun[k]}${exten[o]}`;
+        let element = document.getElementById("domain");
+        element.innerHTML = resultado;
+      }
+    }
+  }
+}
