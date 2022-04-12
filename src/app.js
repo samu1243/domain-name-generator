3d401db924021b822b4b51ce82a528ea20eb2b35
samu1243 <samueldjekki@gmail.com>
2022-04-11 20:31:13 -0400

    domain name generator finalizado

--- src/app.js
+++ src/app.js
@@ -9,15 +9,18 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let exten = [".net", ".com", ".org", ".us"];
+let element = document.getElementById("domain");
+
+let resultadoFinal = "";
 
 //write your code here
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let o = 0; o < exten.length; o++) {
-        let resultado = `${pronoun[i]}${adj[j]}${noun[k]}${exten[o]}`;
-        let element = document.getElementById("domain");
-        element.innerHTML = resultado;
+        let resultado = `<br>${pronoun[i]}${adj[j]}${noun[k]}${exten[o]}`;
+        resultadoFinal += resultado;
+        element.innerHTML = resultadoFinal;
       }
     }
   }
